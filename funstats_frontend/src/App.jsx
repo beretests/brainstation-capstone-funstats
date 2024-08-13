94453b26f7e919b754e29d58b327c9710cae9d6b
Eberechi Omeje <98207440+beretests@users.noreply.github.com>
2024-08-13 07:02:25 -0600

    create home page

--- funstats_frontend/src/App.jsx
+++ funstats_frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>FunStats App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
