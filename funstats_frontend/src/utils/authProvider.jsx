61af7b6524b2a9e3405f08759ee09f59b7c84461
Eberechi Omeje <eb.omeje@gmail.com>
2024-10-08 10:13:36 -0600

    fix page redirect to login on refresh

--- funstats_frontend/src/utils/authProvider.jsx
+++ funstats_frontend/src/utils/authProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
@@ -6,19 +6,44 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [playerId, setPlayerId] = useState(null);
   const [season, setSeason] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    const storedToken = sessionStorage.getItem("JWTtoken");
+    const storedUserId = sessionStorage.getItem("userId");
+    if (storedToken && storedUserId) {
+      setToken(storedToken);
+      setPlayerId(storedUserId);
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
   const login = (userToken, userId) => {
     setToken(userToken);
     setPlayerId(userId);
+    setIsAuthenticated(true);
   };
   const logout = () => {
     setToken(null);
     setPlayerId(null);
+    sessionStorage.clear();
+    setIsAuthenticated(false);
   };
-  const isAuthenticated = !!token && !!playerId;
+  // const isAuthenticated = !!token && !!playerId;
 
   return (
     <AuthContext.Provider
-      value={{ isAuthenticated, login, logout, playerId, setSeason, season }}
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        login,
+        logout,
+        playerId,
+        setSeason,
+        season,
+      }}
     >
       {children}
     </AuthContext.Provider>
--- funstats_frontend/src/utils/privateRoutes.jsx
+++ funstats_frontend/src/utils/privateRoutes.jsx
@@ -1,9 +1,40 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../utils/authProvider";
+import { useEffect, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
 
 const PrivateRoutes = () => {
-  const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  const { isAuthenticated, setIsAuthenticated } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    const storedToken = sessionStorage.getItem("JWTtoken");
+    const storedUserId = sessionStorage.getItem("userId");
+
+    if (storedToken && storedUserId) {
+      // setToken(storedToken);
+      // setPlayerId(storedUserId);
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <Spinner animation="border" role="status" />
+      </div>
+    );
+  }
+
+  return isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/sign_in" state={{ from: location }} />
+  );
 };
 
 export default PrivateRoutes;
